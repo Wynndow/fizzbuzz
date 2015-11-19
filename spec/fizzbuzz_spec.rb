a36754f6eed86eda6f5eea2ddff7896f9d0d33ef
thisdotrob <thisdotrob@gmail.com>
2015-11-19 13:38:32 +0000

    add a test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,15 +2,21 @@ require 'fizzbuzz'
 
 describe 'fizzbuzz' do
 
-  it 'returns \'fizz\' when passed 3' do
-    expect(fizzbuzz(3)).to eq('fizz')
+  context 'returns \'fizz\' if number passed is divisible by 3:' do
+    it 'when passed 3' do
+      expect(fizzbuzz(3)).to eq('fizz')
+    end
+
+    it 'when passed 9' do
+      expect(fizzbuzz(9)).to eq('fizz')
+    end
   end
 
   it 'returns \'buzz\' when passed 5' do
     expect(fizzbuzz(5)).to eq('buzz')
   end
 
-  it 'retuns original number if not 3 or 5' do
+  it 'retuns original number when passed 1' do
     expect(fizzbuzz(1)).to eq(1)
   end
 

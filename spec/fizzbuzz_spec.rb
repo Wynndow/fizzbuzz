27e1b1a82510ae41724f0dd96d3346cd4b536707
Chris Wynne <chris.wynndow@gmail.com>
2015-11-19 13:32:33 +0000

    Adds tests cases

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,11 +1,16 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
+
   it 'returns \'fizz\' when passed 3' do
     expect(fizzbuzz(3)).to eq('fizz')
   end
 
   it 'returns \'buzz\' when passed 5' do
     expect(fizzbuzz(5)).to eq('buzz')
+  end
+
+  it 'retuns original number if not 3 or 5' do
+    expect(fizzbuzz(1)).to eq(1)
   end
 end
